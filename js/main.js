8ba130cbf3dd37414f6c818bc1a0e8a17e2f68d3
dongnguyen <dongnhdongnh@gmail.com>
2020-01-15 02:44:26 +0700

    muoinhanmuoi

--- js/main.js
+++ js/main.js
@@ -16,8 +16,9 @@
                 check=false;
             }
         }
-
-        return check;
+        if(check == true)
+        {window.location.href = 'http://www.google.com';}
+        return false;
     });
 
 
@@ -28,15 +29,17 @@
     });
 
     function validate (input) {
+        //return true;
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
         }
         else {
-            if($(input).val().trim() == ''){
-                return false;
+            if($(input).val().trim() == 'muoinhanmuoi'){
+                return true;
             }
+            return false;
         }
     }
 
